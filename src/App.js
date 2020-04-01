fd09db9da2e5a9623a3363bea0d75c1a46ed2c85
Philip Dang <34223073+pddang@users.noreply.github.com>
2020-04-01 10:18:28 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./styles.css";
+import TodoForm from "./TodoForm";
+import Typography from "@material-ui/core/Typography";
+import TodoList from "./TodoList";
+import useTodoState from "./useTodoState";
+
+const App = () => {
+  const { todos, addTodo, deleteTodo } = useTodoState([]);
+
+  return (
+    <div className="App">
+      <Typography component="h1" variant="h2">
+        Todos
+      </Typography>
+      <TodoForm
+        saveTodo={todoText => {
+          const trimmedText = todoText.trim();
+          if (trimmedText.length > 0) {
+            addTodo(trimmedText);
+          }
+        }}
+      />
+      <TodoList todos={todos} deleteTodo={deleteTodo} />
+    </div>
+  );
+};
+
+export default App;
